e6692f6c45833c9957bd9a65f0e59dd832bdcd32
jdaeth274 <joshdaeth@yahoo.co.uk>
2021-08-24 18:30:41 +0100

    reading in lines of file

--- python/common.py
+++ python/common.py
@@ -6,12 +6,14 @@ def main(input_args):
     """ Main function to run through steps of ABar and comM identification
     """
 
-    seqs = input_args.seqs
+    seq_files = open(input_args.seqs, "r")
+    seq_lines = seq_files.read().splitlines()
+
     if input_args.dna_dir is None:
-        pre_blast_process.make_dna_db(seqs)
+        pre_blast_process.make_dna_db(seq_lines)
 
     if input_args.contig_bounds is None:
-        pre_blast_process.contig_bounds(seqs)
+        pre_blast_process.contig_bounds(seq_lines)
 
 
 
