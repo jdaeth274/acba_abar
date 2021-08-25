c58530ac9b10935d785983a7c3639dc6c8679814
jdaeth274 <joshdaeth@yahoo.co.uk>
2021-08-25 16:07:11 +0100

    adding in the blast sequences

---
+++ python/blast_runs.py
@@ -0,0 +1,38 @@
+import subprocess
+import os
+import sys
+
+
+def blast_runs(tmp_dna_dir, data_dir):
+    """ Function to blast the mfa file against the conserved ends
+        of the AbaRs in the data directory """
+
+    ## Make the blast db for the input .mfa files
+
+    mfa_file = tmp_dna_dir + "/output.mfa"
+    mfa_blast = mfa_file + ".nin"
+    if not os.path.exists(mfa_file):
+        make_db_cmd = "makeblastdb -dbtype nucl -out output.mfa -max_file_sz 2GB -in " + mfa_file
+        try:
+            subprocess.check_output(make_db_cmd, shell=True)
+        except subprocess.SubprocessError:
+            sys.exit("Failed making the BLASTDB for the output mfa file, ensure BLAST is installed")
+
+
+
+    ## Now we'll blast the left end
+    left_end_file = data_dir + "/left_end_conserved.fasta"
+    blast_cmd = "blastn -db " + mfa_file + " -query " + left_end_file + " -outfmt 10 -out left_end_blast.csv -evalue 0.001 -num_alignments 1000000"
+    try:
+        subprocess.check_output(blast_cmd, shell=True)
+    except subprocess.SubprocessError:
+        sys.exit("Failed running BLAST for left end conserved region, ensure BLAST is installed")
+
+    ## Now for the right end
+    left_end_file = data_dir + "/right_end_conserved.fasta"
+    blast_cmd = "blastn -db " + mfa_file + " -query " + left_end_file + " -outfmt 10 -out right_end_blast.csv -evalue 0.001 -num_alignments 1000000"
+    try:
+        subprocess.check_output(blast_cmd, shell=True)
+    except subprocess.SubprocessError:
+        sys.exit("Failed running BLAST for right end conserved region, ensure BLAST is installed")
+
--- python/common.py
+++ python/common.py
@@ -1,6 +1,7 @@
-
+import os
 from python import pre_blast_process
-
+from python import blast_runs
+import re
 
 def main(input_args):
     """ Main function to run through steps of ABar and comM identification
@@ -15,5 +16,11 @@ def main(input_args):
     if input_args.contig_bounds is None:
         pre_blast_process.contig_bounds(seq_lines)
 
+    ## BLAST the fragments now
+    python_dir_name = os.path.dirname(os.path.realpath(__file__))
+    data_dir = re.sub("python", "data", python_dir_name)
+    tmp_dna_dir = "./tmp_dna_lib"
 
+    blast_runs.blast_runs(tmp_dna_dir, data_dir)
 
+    
