bbd264fea218106b2c1ba0e16070a2af1ad54cc3
jdaeth274 <joshdaeth@yahoo.co.uk>
2021-08-30 15:00:43 +0100

    adding in final blast merging

--- python/post_blast_process.py
+++ python/post_blast_process.py
@@ -49,6 +49,7 @@ def extract_hits(out_name):
                 right_hits = right_match[right_match['sstart'] >= (row.loc['send'] - 150000)]
                 if right_hits.empty:
                     continue
+            print(type(right_hits))
             id.append(row.loc['subject'])
             hit_start.append(row.loc['sstart'])
             hit_end.append(right_hits.loc[0,"send"])
